15bac8050ca235f3ec9bcb1b4057928bd97b013f
chan-max <jackieontheway666@gmail.com>
2025-07-23 08:40:00 +0800

    dev

--- .vitepress/theme/components/vuetheme/vitepress/components/VPNavBarTitle.vue
+++ .vitepress/theme/components/vuetheme/vitepress/components/VPNavBarTitle.vue
@@ -2,7 +2,7 @@
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2025-07-03 07:33:12
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2025-07-23 07:15:49
+ * @LastEditTime: 2025-07-23 08:37:53
  * @FilePath: /design-server/Users/jackie/workspace/yishe-docs/.vitepress/theme/components/vuetheme/vitepress/components/VPNavBarTitle.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -10,7 +10,7 @@
   <a class="VPNavBarTitle" href="/">
     <slot name="navbar-title">
       <img class="logo" width="30" height="30" src="/images/logo-pure.png" alt="logo" />
-      <div class="text">衣设文档 <small style="font-style: italic;opacity:.5; font-weight: bold;font-size: 10px;">docs of 1s.design </small></div>
+      <div class="text">衣设文档 <small style="font-style: italic;opacity:.5; font-weight: bold;font-size: 10px;margin-left: 4px;">docs of 1s.design </small></div>
     </slot>
   </a>
 </template>
