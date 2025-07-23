7152f36dd12a90000275365937711b6dc2bd0874
chan-max <jackieontheway666@gmail.com>
2025-07-23 08:15:56 +0800

    dev

--- .vitepress/headerMdPlugin.ts
+++ .vitepress/headerMdPlugin.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: chan-max jackieontheway666@gmail.com
+ * @Date: 2025-06-13 20:22:44
+ * @LastEditors: chan-max jackieontheway666@gmail.com
+ * @LastEditTime: 2025-07-23 08:06:47
+ * @FilePath: /design-server/Users/jackie/workspace/yishe-docs/.vitepress/headerMdPlugin.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /**
  * A markdown-it plugin to support custom header metadata
  * Headers that end with * are Options API only
